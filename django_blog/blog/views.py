fcc7e4202222ff24eebd420e2e72edcfd8faf1cc
Mina Markos <minalxr123@gmail.com>
2024-12-08 18:54:26 +0200

    update views

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -4,21 +4,26 @@ from django.contrib import messages
 from django.contrib.auth.views import LoginView
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
-from .forms import UserUpdateForm, ProfileUpdateForm, CommentForm
-from .models import Profile, Post, Comment
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, View
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .forms import UserUpdateForm, ProfileUpdateForm, CommentForm
+from .models import Profile, Post, Comment
 
+
+# Authentication Views
 class CustomLoginView(LoginView):
-    template_name ='blog/login.html'
+    template_name = 'blog/login.html'
 
-class LogoutView():
+
+class LogoutView:
+    @staticmethod
     def logout_view(request):
         if request.method == 'POST':
             logout(request)
             return redirect('login')
         return render(request, 'blog/logout.html')
+
 
 def register_view(request):
     if request.method == 'POST':
@@ -39,9 +44,7 @@ def profile_view(request):
 
     if request.method == 'POST':
         user_form = UserUpdateForm(request.POST, instance=request.user)
-        profile_form = ProfileUpdateForm(
-            request.POST, request.FILES, instance=request.user.profile
-        )
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
 
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
@@ -52,13 +55,11 @@ def profile_view(request):
         user_form = UserUpdateForm(instance=request.user)
         profile_form = ProfileUpdateForm(instance=request.user.profile)
 
-    context = {
-        'user_form': user_form,
-        'profile_form': profile_form,
-    }
-
+    context = {'user_form': user_form, 'profile_form': profile_form}
     return render(request, 'blog/profile.html', context)
 
+
+# Post Views
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
@@ -85,6 +86,7 @@ class PostDetailView(View):
         comments = post.comments.all()
         return render(request, 'blog/post_details.html', {'post': post, 'comments': comments, 'form': form})
 
+
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'blog/post_form.html'
@@ -94,40 +96,42 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
+
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
-    fields = ['title', 'content']  # Specify the fields to include in the form
+    fields = ['title', 'content']
     template_name = 'blog/post_form.html'
-    success_url = reverse_lazy('post-list')    # Ensure only the author can edit their post
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
+    success_url = reverse_lazy('post-list')
 
-    # Automatically set the author when the form is valid
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'blog/post_confirm_delete.html'
     success_url = reverse_lazy('post-list')
-    
-    def test_func(self):
-        post = self.get_object()
-        return self.request.user == post.author
-    
 
-class CommentView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+
+# Comment Views
+class CommentCreateView(LoginRequiredMixin, CreateView):
     model = Comment
     form_class = CommentForm
     template_name = 'blog/comment_form.html'
-    success_url = reverse_lazy('post-list')
 
-    def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
-    
+    def form_valid(self, form):
+        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
+        form.instance.post = post
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+
 
 class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Comment
@@ -135,14 +139,15 @@ class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     template_name = 'blog/comment_form.html'
 
     def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
+        return self.request.user == self.get_object().author
+
 
 class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Comment
     template_name = 'blog/comment_confirm_delete.html'
+
     def get_success_url(self):
-        return redirect('post-details', pk=self.object.post.pk)
+        return reverse_lazy('post-detail', kwargs={'pk': self.object.post.pk})
+
     def test_func(self):
-        comment = self.get_object()
-        return self.request.user == comment.author
+        return self.request.user == self.get_object().author
