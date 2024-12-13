484f73c785c811bb5ba6015f904533ec32d1ee16
Mina Markos <minalxr123@gmail.com>
2024-12-13 22:14:11 +0200

    edit this API Serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,5 +1,46 @@
 from rest_framework import serializers
 from .models import CustomUser
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model, authenticate
+
+User = get_user_model()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        mode = User
+        fields = '__all__'
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            bio=validated_data['bio'],
+            profile_picture=validated_data['profile_picture']
+        )
+        Token.objects.create(user=user)
+        return user
+    
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        if username is None or password is None:
+            raise serializers.ValidationError('Please provide both username and password')
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            raise serializers.ValidationError('Invalid Credentials')
+
+        data['user'] = user
+        data['token'], _ = Token.objects.get_or_create(user=user)[0]
+        return data
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -5,40 +5,29 @@ from rest_framework.authtoken.models import Token
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import authenticate
 from .models import CustomUser
-from .serializers import UserSerializer
+from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
 
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-        bio = request.data.get('bio')
-        profile_picture = request.data.get('profile_picture')
-
-        if username is None or password is None:
-            return Response({'error': 'Please provide both username and password'}, status=400)
-
-        user = CustomUser.objects.create_user(username=username, password=password, bio=bio, profile_picture=profile_picture)
-        Token.objects.create(user=user)
-        return Response({'detail': 'User created successfully'}, status=201)
-
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                'user': RegisterSerializer(user).data,
+                'token': Token.objects.get(user=user).key
+            })
+        return Response(serializer.errors, status=400)
 class LoginView(APIView):
     permission_classes = [AllowAny]
-    success_url = '/'
-
-    def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
-
-        if username is None or password is None:
-            return Response({'error': 'Please provide both username and password'}, status=400)
-
-        user = authenticate(username=username, password=password)
-
-        if not user:
-            return Response({'error': 'Invalid Credentials'}, status=400)
-
-        token, _ = Token.objects.get_or_create(user=user)
-        return Response({'token': token.key}, status=200)
     
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            return Response({
+                'user': UserSerializer(user).data,
+                'token': serializer.validated_data['token']
+            })
+        return Response(serializer.errors, status=400)
