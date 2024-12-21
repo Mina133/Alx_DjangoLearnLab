fbc805018283ceb031f13b3cee04c6ecdfbfd93b
Mina Markos <minalxr123@gmail.com>
2024-12-21 19:32:06 +0200

    user_id insted of pk

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -8,6 +8,6 @@ from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('users/<int:pk>/follow/', FollowUserView.as_view(), name='follow-user'),
-    path('users/<int:pk>/unfollow/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('users/<int:user_id>/follow/', FollowUserView.as_view(), name='follow-user'),
+    path('users/<int:user_id>/unfollow/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
