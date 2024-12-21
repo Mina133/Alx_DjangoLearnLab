924baf8493d949ab07b661a07d2aa75806cb37b0
Mina Markos <minalxr123@gmail.com>
2024-12-21 19:43:32 +0200

    IDK

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -8,6 +8,6 @@ from rest_framework.routers import DefaultRouter
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('follow//<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow//<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
